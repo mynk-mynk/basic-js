9246e4b979a282477aaf9c797f86a8fe479bf263
Romashkina <da_shootka@mail.ru>
2022-04-28 20:04:07 +0200

    feat: implement decision for common-character-count

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  let obj1 = {};
+  let obj2 = {};
+  let count = 0;
+
+  obj1 = countSymbols(s1, obj1);
+  obj2 = countSymbols(s2, obj2);
+
+  if (!obj1 || !obj2) return 0;
+
+  for (let key in obj1) {
+    if (obj2.hasOwnProperty(key)) count += Math.min(obj1[key], obj2[key]);
+  }
+
+  return count;
+}
+
+function countSymbols(str, obj) {
+
+  for (let i = 0; i < str.length; i++) {
+    obj.hasOwnProperty(str[i]) ? obj[str[i]] += 1 : obj[str[i]] = 1;
+  }
+  return obj;
 }
 
 module.exports = {
