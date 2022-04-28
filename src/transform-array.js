cf4c404fbbe3933f936a4ed8374015e82f235caa
Romashkina <da_shootka@mail.ru>
2022-04-28 11:10:42 +0200

    feat: implement decision for carbon-dating, count-cats, dream-team, hanoi-tower, transform-array and what-season

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,53 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!")
+  }
+
+  let transformedArr = [];
+  const commands = ['--discard-next', '--discard-prev', '--double-next', '--double-prev'];
+
+  for (let i = 0; i < arr.length; i++) {
+
+    switch (arr[i]) {
+
+      case '--discard-next':
+        i++;
+        break;
+
+      case '--discard-prev':
+        if (transformedArr[transformedArr.length - 1] === arr[i - 1]) {
+          transformedArr.pop();
+        }
+        break;
+
+      case '--double-next':
+        if (arr[i + 1] && !commands.includes(arr[i + 1])) {
+          transformedArr.push(arr[i + 1]);
+        }
+        break;
+
+      case '--double-prev':
+        if (arr[i - 1] && arr[i - 2] !== '--discard-next') {
+          transformedArr.push(arr[i - 1]);
+        }
+        break;
+
+      default:
+        transformedArr.push(arr[i]);
+    }
+  }
+
+  return transformedArr;
 }
+
+// Failing test case
+// transform([1, 2, 3, '--discard-next', 1337, '--discard-prev', 4, 5])
+// Expected: [1,2,3,4,5]
+// Actual: [1,2,4,5]
 
 module.exports = {
   transform
