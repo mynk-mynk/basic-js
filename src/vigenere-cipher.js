9b9d9208e7f92ff33dac01654571db2653846968
mynk-mynk <da_shootka@mail.ru>
2022-05-02 23:49:26 +0200

    feat: implement decision for vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,85 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(arg) {
+
+    if (arg === false) {
+      this.style = -1;
+
+    } else {
+      this.style = 1;
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(str, key) {
+
+    if (!str || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    
+    str = str.toLowerCase();
+    key = key.toLowerCase();
+    let result = '';
+    let j = 0;
+    
+    for (let i = 0; i < str.length; i++) {
+      
+      if (str[i].charCodeAt() < 97 || str[i].charCodeAt() > 122) {
+        result += str[i];
+        continue;
+      }
+
+      let charCode = str[i].charCodeAt() + key[j % key.length].charCodeAt() - 97;
+
+      if (charCode < 123) {
+        result += String.fromCharCode(charCode);
+        j++;
+
+      } else {
+        result += String.fromCharCode(charCode % 123 + 97);
+        j++;
+      }
+    }
+
+    return this.style === 1 ? result.toUpperCase() : result.split('').reverse().join('').toUpperCase();
+  }
+
+
+  decrypt(code, key) {
+
+    if (!code || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    code = code.toLowerCase();
+    key = key.toLowerCase();
+    let result = '';
+    let j = 0;
+
+    for (let i = 0; i < code.length; i++) {
+
+      if (code[i].charCodeAt() < 97 || code[i].charCodeAt() > 122) {
+        result += code[i];
+        continue;
+      }
+
+      let charCode = code[i].charCodeAt() + 26 - key[j % key.length].charCodeAt() + 97;
+
+      if (charCode < 123) {
+        result += String.fromCharCode(charCode);
+        j++;
+
+      } else {
+        result += String.fromCharCode(charCode % 123 + 97);
+        j++;
+      }
+    }
+
+    return this.style === 1 ? result.toUpperCase() : result.split('').reverse().join('').toUpperCase();
   }
 }
+
 
 module.exports = {
   VigenereCipheringMachine
